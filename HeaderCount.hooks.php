851d1f52794e5db23f3ddd046b7cc6d21e4728c9
Eli Clemente Gordillo Foster <elifosterwy@gmail.com>
2024-02-05 06:12:03 -0800

    Update for MediaWiki 1.39

--- HeaderCount.hooks.php
+++ HeaderCount.hooks.php
@@ -1,26 +1,30 @@
 <?php
 
-class HeaderCountHooks {
-    public static function setupParser(Parser &$parser) {
+use MediaWiki\Hook\ParserFirstCallInitHook;
+use MediaWiki\Revision\RevisionRecord;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\Revision\SlotRecord;
+
+class HeaderCountHooks implements ParserFirstCallInitHook {
+	public function onParserFirstCallInit($parser) {
         $parser->setFunctionHook('headcount', 'HeaderCountHooks::renderHeadCount');
     }
 
-    public static function renderHeadCount(Parser &$parser, $page = '', $level = '') {
-        if (empty($page)) {
-            $title = $parser->getTitle();
-        } else {
-            $title = Title::newFromText($page);
-            if (!$title->exists()) {
-                return "'''$title does not exist.'''";
-            }
+    public static function renderHeadCount(Parser $parser, $page = '', $level = '') {
+        $title = empty($page) ? $parser->getPage()->getText() : $page;
+        $page = MediaWikiServices::getInstance()->getPageStore()->getExistingPageByText($title);
+
+        if ($page === null) {
+            return "'''$title does not exist.'''";
         }
 
-        $rev = Revision::newFromTitle($title);
+        $rev = MediaWikiServices::getInstance()->getRevisionStore()->getRevisionByTitle($page);
+
         if ($rev === null) {
             return "'''Could not retrieve revision from $title.'''";
         }
 
-        $content = $rev->getContent(Revision::RAW);
+        $content = $rev->getContent(SlotRecord::MAIN);
         if ($content === null) {
             return "'''Could not extract text from $title.'''";
         }
